86e5f7d0eb05dc969d36858aa6a37d32ced5df51
dazuma15 <dazuma15@gmail.com>
2014-03-24 20:23:49 -0400

    welcome#show issue fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
   resources :welcome
-  #get '/welcome/show', to: 'welcome#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
