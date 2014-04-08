ab7a93f503c38db2fe626f564bbc4864e9dd768d
dazuma15 <dazuma15@gmail.com>
2014-04-08 16:57:32 -0400

    Design changes, sign_in page made mobile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     if mobile_device?
       @posts = Post.all
     else
-    @posts = Post.paginate(:page => params[:page], :per_page => 5)
+    @posts = Post.paginate(:page => params[:page], :per_page => 12).order("created_at DESC")
     end
   end
   
